630708e814efe64c7ddd50d570a7d886d63fa2c5
Leon Luithlen <leontimnaluithlen@gmail.com>
2023-05-05 16:47:39 +0200

    Add evaluate_gec.py

---
+++ cloud/scripts/evaluate_gec.py
@@ -0,0 +1,87 @@
+import json
+import os
+import typer
+import shutil
+import contextlib
+
+import numpy as np
+from lightgbm import LGBMClassifier
+from helpers.load_dataset import load_bank_dataset
+from sklearn.model_selection import cross_val_score
+from gecs100.gec import GEC
+from sklearn.model_selection import RandomizedSearchCV
+
+OUTPUT_LOCATION = "./output"
+
+
+N_ITER = 100
+
+def prepare_folder():
+
+    if os.path.exists(OUTPUT_LOCATION) and os.path.isdir(OUTPUT_LOCATION):
+        shutil.rmtree(OUTPUT_LOCATION)
+
+    is os.path.exists(f"{OUTPUT_LOCATION}.zip"):
+        os.remove(f"{OUTPUT_LOCATION}.zip")
+
+    os.makedirs(OUTPUT_LOCATION)
+    os.makedirs(f"{OUTPUT_LOCATION}/figures")
+
+def fit_random_search(X, y, gec):
+
+    classifier = LGBMClassifier()
+    hyperparams = dict(gec.categorical_hyperparameters[:1] + gec.real_hyperparameters)
+    random_search = RandomizedSearchCV(classifier, hyperparams, n_iter=N_ITER)
+    random_search.fit(X, y)
+
+    return random_search
+
+
+def benchmark_against_random_search(X_eval, y_eval, gec, random_search):
+
+    with contextlib.redirect_stdout(None):
+
+        knn_bayes = LGBMClassifier(**gec.best_params_)
+        score_bayes = np.mean(cross_val_score(knn_bayes, X_eval, y_eval, cv=5))
+        knn_gs = LGBMClassifier(**random_search.best_params_)
+        score_gs = np.mean(cross_val_score(knn_gs, X_eval, y_eval, cv=5))
+        knn_default = LGBMClassifier()
+        score_default = np.mean(cross_val_score(knn_default, X_eval, y_eval, cv=5))
+
+    return {
+        "bayesian": score_bayes,
+        "random search": score_gs,
+        "default": score_default,
+    }
+
+
+app = typer.Typer(name="run GEC benchmarking")
+
+
+@app.command()
+def run(dataset: str = "bank"):
+    prepare_folder()
+
+    gec = GEC()
+
+    if dataset == "bank":
+        X, y = load_bank_dataset(0.2)
+    else:
+        raise Exception(f"dataset {dataset} is not available")
+
+    gec.fit(X, y, N_ITER)
+
+    gec.serialise(f"{OUTPUT_LOCATION}/gec.json")
+    gec.save_figs(f"{OUTPUT_LOCATION}/figures/fig")
+
+    random_search = fit_random_search(X, y, gec)
+    benchmark = benchmark_against_random_search(X, y, gec, random_search)
+
+    with open(f"{OUTPUT_LOCATION}/benchmark.json", "w") as f:
+        f.write(json.dumps(benchmark))
+
+    shutil.make_archive(OUTPUT_LOCATION, "zip", "./")
+
+
+if __name__ == "__main__":
+    app()
---
+++ cloud/scripts/helpers/load_dataset.py
@@ -0,0 +1,45 @@
+import pandas as pd
+import numpy as np
+
+from sklearn.preprocessing import OneHotEncoder
+
+
+def load_bank_dataset(share=0.1):
+
+    path = "/home/leon/learning/gaussian-processes/bank/bank-full.csv"
+    data = pd.read_csv(path, sep=";")
+    one_hot_encode = ["job", "marital", "education", "contact", "poutcome", "month"]
+    binary = ["default", "housing", "loan", "y"]
+
+    enc = OneHotEncoder()
+
+    def yesNoBinary(column):
+        return pd.DataFrame(
+            [1 if value == "yes" else 0 for value in column], columns=[column.name]
+        )
+
+    one_hot = pd.DataFrame(
+        enc.fit_transform(data[one_hot_encode]).toarray(),
+        columns=enc.get_feature_names_out(one_hot_encode),
+    )
+    data2 = pd.concat(
+        [one_hot]
+        + [
+            yesNoBinary(data[col]) if col in binary else data[col]
+            for col in data.columns
+        ],
+        1,
+    ).drop(one_hot_encode, 1)
+    X, y = data2.values[:, :-1], data2.values[:, -1]
+    np.random.seed(101)
+    ind = np.random.uniform(0, 1, X.shape[0]) < share
+    X = X[ind, :]
+    y = y[ind]
+
+    X_pos = X[y == 1, :]
+    y_pos = y[y == 1]
+
+    X_reweighted = np.concatenate([X] + [X_pos] * 3, axis=0)
+    y_reweighted = np.concatenate([y] + [y_pos] * 3)
+
+    return (X_reweighted, y_reweighted)
