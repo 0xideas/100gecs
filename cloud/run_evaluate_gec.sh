598c497e927945f461e887a3b66c02d6c6924ddd
Leon Luithlen <leontimnaluithlen@gmail.com>
2023-05-06 22:41:29 +0200

    Use poetry to run evaluate_gec.py

--- cloud/run_evaluate_gec.sh
+++ cloud/run_evaluate_gec.sh
@@ -2,8 +2,8 @@ cd cloud/terraform
 terraform apply
 
 cd ../..
-scp -i ~/auth/leon_ec2 cloud/config/config /home/ubuntu/config
-ssh -i ~/auth/leon_ec2 user@remote.host nohup python3 100gecs/cloud/scripts/evaluate_gec.py
+scp -i ~/auth/leon_ec2 cloud/config/config user@remote.host:/home/ubuntu/config
+ssh -i ~/auth/leon_ec2 user@remote.host nohup poetry run python3 100gecs/cloud/scripts/evaluate_gec.py
 
 cd cloud/terraform
 terraform destroy
