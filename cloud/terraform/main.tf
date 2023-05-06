ab33e13dd8a7c397ccb409cceb5247ba9f760303
Leon Luithlen <leontimnaluithlen@gmail.com>
2023-05-06 21:29:49 +0200

    First draft terraform

---
+++ cloud/terraform/main.tf
@@ -0,0 +1,12 @@
+provider "aws" {
+    shared_credentials_file = ~/.aws/credentials"
+    region = var.aws_region
+}
+
+resource "aws_instance" "ec2_instance" {
+    ami = "${var.ami_id}"
+    count = "${var.number_of_instances}"
+    subnet_id = "${var.subnet_id}"
+    instance_type = "${var.instance_type}"
+    key_name = "${var.ami_key_pair_name}"
+} 
---
+++ cloud/terraform/variables.tf
@@ -0,0 +1,33 @@
+variable "access_key" {
+        description = "Access key to AWS console"
+}
+variable "secret_key" {
+        description = "Secret key to AWS console"
+}
+
+
+variable "instance_name" {
+        default = "run-evaluate-gec-inst"
+}
+
+variable "instance_type" {
+        default = "t2.micro"
+}
+
+variable "subnet_id" {
+        description = "The VPC subnet the instance(s) will be created in"
+}
+
+variable "ami_id" {
+        description = "The AMI to use"
+}
+
+variable "number_of_instances" {
+        description = "number of instances to be created"
+        default = 1
+}
+
+
+variable "ami_key_pair_name" {
+        description = "ami key pair name"
+}
