1e1431fb1059f9335b73c805b7de7ebbb2797f90
Leon Luithlen <leontimnaluithlen@gmail.com>
2023-05-07 13:23:45 +0200

    run_evaluate_gec works

--- cloud/run_evaluate_gec.sh
+++ cloud/run_evaluate_gec.sh
@@ -3,9 +3,12 @@ terraform apply -auto-approve
 
 sleep 1m
 cd ..
-bash ./set_env_variables.sh
-# scp -i ~/auth/leon_ec2.pem config/config ubuntu@$REMOTE_HOST:/home/ubuntu/config
-ssh -i ~/auth/leon_ec2.pem ubuntu@$REMOTE_HOST bash run.sh
+ssh-keygen -f "/home/leon/.ssh/known_hosts" -R $REMOTE_IP
+
+echo "setting env variables"
+source ./set_env_variables.sh
+# scp -i ~/auth/leon_ec2.pem -o "StrictHostKeyChecking no" config/config ubuntu@$REMOTE_HOST:/home/ubuntu/config
+ssh -i ~/auth/leon_ec2.pem -o "StrictHostKeyChecking no" ubuntu@$REMOTE_HOST bash run.sh
 
 sleep 10s
 cd terraform
