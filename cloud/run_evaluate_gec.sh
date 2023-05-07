83f7304c25842b85eb103200a15f0ca7ae856fb7
Leon Luithlen <leontimnaluithlen@gmail.com>
2023-05-07 11:56:28 +0200

    Ignore terraform artifacts

--- cloud/run_evaluate_gec.sh
+++ cloud/run_evaluate_gec.sh
@@ -1,9 +1,9 @@
 cd cloud/terraform
-terraform apply
+terraform apply -auto-approve
 
 cd ../..
 scp -i ~/auth/leon_ec2 cloud/config/config user@remote.host:/home/ubuntu/config
-ssh -i ~/auth/leon_ec2 user@remote.host nohup poetry run python3 100gecs/cloud/scripts/evaluate_gec.py
+ssh -i ~/auth/leon_ec2 ubuntu@remote.host nohup poetry run python3 100gecs/cloud/scripts/evaluate_gec.py
 
 cd cloud/terraform
-terraform destroy
+terraform destroy -auto-approve
