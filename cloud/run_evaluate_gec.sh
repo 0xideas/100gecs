134481e276f928169b00418a4b77a9d4575bc5e3
Leon Luithlen <leontimnaluithlen@gmail.com>
2023-05-10 13:13:22 +0200

    Pass more info as env variables

--- cloud/run_evaluate_gec.sh
+++ cloud/run_evaluate_gec.sh
@@ -3,13 +3,14 @@ terraform apply -auto-approve
 
 sleep 1m
 cd ..
-ssh-keygen -f "/home/leon/.ssh/known_hosts" -R $REMOTE_IP
 
 echo "setting env variables"
 source ./set_env_variables.sh
 # scp -i ~/auth/leon_ec2.pem -o "StrictHostKeyChecking no" config/config ubuntu@$REMOTE_HOST:/home/ubuntu/config
-ssh -i ~/auth/leon_ec2.pem -o "StrictHostKeyChecking no" ubuntu@$REMOTE_HOST bash run.sh
+ssh -i $KEY_PATH -o "StrictHostKeyChecking no" ubuntu@$REMOTE_HOST bash run.sh
 
 sleep 10s
 cd terraform
 terraform destroy -auto-approve
+
+ssh-keygen -f $SSH_HOST_PATH -R $REMOTE_IP
