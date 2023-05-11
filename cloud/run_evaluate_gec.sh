e5ce15ef0db20c31695254ed9dddb8e0e17bb334
Leon Luithlen <leontimnaluithlen@gmail.com>
2023-05-11 13:56:13 +0200

    Adapt run_evaluate_gec.sh to screen

--- cloud/run_evaluate_gec.sh
+++ cloud/run_evaluate_gec.sh
@@ -1,19 +1,28 @@
 cd terraform
 terraform apply -auto-approve
 
-sleep 1m
 cd ..
 
 echo "setting env variables"
-source ./set_env_variables.sh
+source ./set_env_variables.sh $1 $2
+ssh-keygen -f $SSH_HOST_PATH -R $REMOTE_IP
 scp -i $KEY_PATH -o "StrictHostKeyChecking no" config/config.json ubuntu@$REMOTE_HOST:/home/ubuntu/config.json
-ssh -i $KEY_PATH -o "StrictHostKeyChecking no" ubuntu@$REMOTE_HOST bash run.sh $1 $2
+ssh -t -i $KEY_PATH -o "StrictHostKeyChecking no" ubuntu@$REMOTE_HOST bash run.sh $AWS_ACCESS_KEY_ID $AWS_SECRET_ACCESS_KEY
+
+current_screens_length=1
+while [ $current_screens_length != 0 ]
+do
+    echo -n "."
+    sleep 10s
+    current_screens=$(screen -list | grep "gec")
+    current_screens_length=${#current_screens}
+done
+
 
 sleep 10s
 cd terraform
 terraform destroy -auto-approve
 
-ssh-keygen -f $SSH_HOST_PATH -R $REMOTE_IP
 
 
 #scp -i $KEY_PATH -o "StrictHostKeyChecking no" ubuntu@$REMOTE_HOST:/home/ubuntu/gec.json gec.json
