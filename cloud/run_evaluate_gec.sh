48bda13de81f8ca23d940836a2f81db3165eb15d
Leon Luithlen <leontimnaluithlen@gmail.com>
2023-05-11 19:17:23 +0200

    Add nohup to execution

--- cloud/run_evaluate_gec.sh
+++ cloud/run_evaluate_gec.sh
@@ -7,7 +7,7 @@ echo "setting env variables"
 source ./set_env_variables.sh $1 $2
 ssh-keygen -f $SSH_HOST_PATH -R $REMOTE_IP
 scp -i $KEY_PATH -o "StrictHostKeyChecking no" config/config.json ubuntu@$REMOTE_HOST:/home/ubuntu/config.json
-ssh -t -i $KEY_PATH -o "StrictHostKeyChecking no" ubuntu@$REMOTE_HOST bash run.sh $AWS_ACCESS_KEY_ID $AWS_SECRET_ACCESS_KEY
+ssh -t -i $KEY_PATH -o "StrictHostKeyChecking no" ubuntu@$REMOTE_HOST nohup bash run.sh $AWS_ACCESS_KEY_ID $AWS_SECRET_ACCESS_KEY
 
 current_screens_length=1
 while [ $current_screens_length != 0 ]
