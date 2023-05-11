8080dfce9f5bc16050740e95d18cf5dba60ed0c5
Leon Luithlen <leontimnaluithlen@gmail.com>
2023-05-11 14:43:04 +0200

    Adapt for loop to use ssh

--- cloud/run_evaluate_gec.sh
+++ cloud/run_evaluate_gec.sh
@@ -13,13 +13,14 @@ current_screens_length=1
 while [ $current_screens_length != 0 ]
 do
     echo -n "."
-    sleep 10s
-    current_screens=$(screen -list | grep "gec")
+    sleep 1s
+    current_screens=$(ssh -t -i $KEY_PATH -q -o "StrictHostKeyChecking no" ubuntu@$REMOTE_HOST screen -list | grep "gec")
     current_screens_length=${#current_screens}
 done
 
+echo "loop done, quitting in 1m"
 
-sleep 10s
+sleep 1m
 cd terraform
 terraform destroy -auto-approve
 
