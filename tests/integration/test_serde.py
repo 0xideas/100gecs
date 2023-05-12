b98b69d911f34e2798ffaab440db6a8efa0b3a0b
Leon Luithlen <leontimnaluithlen@gmail.com>
2023-05-12 12:23:34 +0200

    Fix test_serde

--- tests/integration/test_serde.py
+++ tests/integration/test_serde.py
@@ -8,21 +8,10 @@ def serialisation_path():
     return "tests/data/gec.json"
 
 
-def test_serde(gec, serialisation_path):
+def test_serde(gec, serialisation_path, X, y):
     gec.serialise(serialisation_path)
 
-    gec2 = GEC.deserialise(serialisation_path)
+    gec2 = GEC.deserialise(serialisation_path, X, y)
 
     for k, v in gec._get_representation().items():
-        if not str(v) == str(getattr(gec2, k)):
-            str1 = np.array(list(str(v)))
-            str2 = np.array(list(str(getattr(gec2, k))))
-            comp = str1 != str2
-            print(k)
-            print(comp.shape)
-            print(np.sum(comp))
-            print(list(comp.astype(int)))
-            print(str1[comp])
-            print(str2[comp])
-
-            assert False
+        assert str(v) == str(getattr(gec2, k))
