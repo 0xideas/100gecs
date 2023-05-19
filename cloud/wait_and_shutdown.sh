bfcd3b02e1f2738eaa54547f78cfd2bafc6c9d03
Leon Luithlen <leontimnaluithlen@gmail.com>
2023-05-19 10:45:22 +0200

    Make wait_and_shutdown check again

--- cloud/wait_and_shutdown.sh
+++ cloud/wait_and_shutdown.sh
@@ -7,11 +7,18 @@ do
     process_match_length=${#process_text}
     if [[ process_match_length -eq "0" ]]
     then
-        break
+        echo "loop done strike one: checking again in 3m"
+        sleep 3m
+        process_text=$(ssh -t -i $KEY_PATH -q -o "StrictHostKeyChecking no" ubuntu@$REMOTE_HOST ps -u ubuntu | grep "python3")
+        process_match_length=${#process_text}
+        if [[ process_match_length -eq "0" ]]
+        then
+            break
+        fi
     fi
 done
 
-echo "loop done, quitting in 1m"
+echo "loop done strike two, quitting in 1m"
 
 sleep 1m
 cd terraform
