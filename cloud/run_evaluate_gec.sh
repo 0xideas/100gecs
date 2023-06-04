25ffd3996664edcd0c546277cc94e2f8a08041bd
Leon Luithlen <leontimnaluithlen@gmail.com>
2023-06-04 13:01:29 +0200

    Remove superflous bash arguments

--- cloud/run_evaluate_gec.sh
+++ cloud/run_evaluate_gec.sh
@@ -4,7 +4,7 @@ terraform apply -auto-approve
 cd ..
 
 echo "setting env variables"
-source ./set_env_variables.sh $1 $2
+source ./set_env_variables.sh
 ssh-keygen -f $SSH_HOST_PATH -R $REMOTE_HOST
 scp -i $KEY_PATH -o "StrictHostKeyChecking no" config/config.json ubuntu@$REMOTE_HOST:/home/ubuntu/config.json
 ssh -t -i $KEY_PATH -o "StrictHostKeyChecking no" ubuntu@$REMOTE_HOST nohup bash run.sh $AWS_ACCESS_KEY_ID $AWS_SECRET_ACCESS_KEY &
