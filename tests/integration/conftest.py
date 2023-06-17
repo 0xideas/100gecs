17d31c97c586b934a843e71137cbf7624a5e21a8
Leon Luithlen <leontimnaluithlen@gmail.com>
2023-06-17 17:57:40 +0200

    Speed up test

--- tests/integration/conftest.py
+++ tests/integration/conftest.py
@@ -41,5 +41,31 @@ def y(X, seed):
 @pytest.fixture(scope="session")
 def gec(X, y):
     gec = GEC()
-    gec.fit(X, y, 2)
+    gec.set_gec_hyperparameters({
+        "l": 1.0,
+        "l_bagging": 0.1,
+        "hyperparams_acquisition_percentile": 0.7,
+        "bagging_acquisition_percentile": 0.7,
+        "bandit_greediness": 1.0,
+        "n_random_exploration": 10,
+        "n_sample": 100,
+        "n_sample_initial": 100,
+        "best_share": 0.2,
+        "hyperparameters": [
+            "learning_rate",
+            "max_bin",
+            "lambda_l1",
+            "lambda_l2",
+            "min_data_in_leaf",
+            "feature_fraction",
+        ],
+        "randomize": True,
+        "estimators_leaves": {
+            "n_exploitation": 5,
+            "share_exploitation": 0.1,
+            "exploration_n_estimators": 10,
+            "exploration_num_leaves": 10
+        }
+    })
+    gec.fit(X, y, 2, 10, 10)
     return gec
