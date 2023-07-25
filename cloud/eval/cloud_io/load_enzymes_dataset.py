e1033eb7d442d49351295506ba80819662ee9e6a
0xideas <leontimnaluithlen@gmail.com>
2023-07-25 09:19:57 +0200

    Add enzymes data loading (#10)

---
+++ cloud/eval/cloud_io/load_enzymes_dataset.py
@@ -0,0 +1,11 @@
+import numpy as np
+import pandas as pd
+
+def load_enzymes_dataset(path, share, target):
+    data = pd.read_csv(path, sep=",", header=0)
+
+    y = data[f"EC{target}"].values
+    X =  data.iloc[:,1:-6].values
+
+    return(X, y)
+
