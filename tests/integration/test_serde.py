e50343f7fde8134e69ac61d2ebbf048762f3c4ee
0xideas <leontimnaluithlen@gmail.com>
2023-08-02 01:03:31 +0200

    Rf/readme2 (#12)
    * Add underscore to attributes
    
    * Bug fix
    
    * Fix bugs on getting best_params_ and on setting subsample and subsample_freq
    
    * Adapt README to be more complete
    
    * Add return
    
    * bump version

--- tests/integration/test_serde.py
+++ tests/integration/test_serde.py
@@ -9,4 +9,5 @@ def test_serde(gec, gec_is_serialised, serialisation_path, X, y):
     gec2 = GEC.deserialize(serialisation_path, X, y)
 
     for k, v in gec._get_representation().items():
-        assert str(v) == str(getattr(gec2, k))
+        v2 = getattr(gec2, f"{k}_")
+        assert str(v) == str(v2), f"{k} - {v} != {v2}"
