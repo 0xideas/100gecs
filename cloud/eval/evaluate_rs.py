8ffc2f06a3f657ea691e105179fd56f40c9de976
Leon Luithlen <leontimnaluithlen@gmail.com>
2023-08-02 01:03:11 +0200

    bump version

--- cloud/eval/evaluate_rs.py
+++ cloud/eval/evaluate_rs.py
@@ -15,7 +15,7 @@ from sklearn.model_selection import RandomizedSearchCV, cross_val_score
 
 from gecs.gec import GEC
 
-VERSION = 31
+VERSION = 32
 SCORE_LOCATION = f"eval/scores/v={VERSION}"
 BUCKET = "100gecs"
 
